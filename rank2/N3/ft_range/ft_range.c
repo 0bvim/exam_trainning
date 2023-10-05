6546a3ca724c35bf6cde9c340433120bff693b6d
vde-frei <vde-frei@student.42sp.org.br>
2023-10-05 20:42:56 -0300

    fix: updated ft_range  Changes to be committed: 	modified:   N3/ft_range/ft_range.c

--- rank2/N3/ft_range/ft_range.c
+++ rank2/N3/ft_range/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei vde-frei@student.42sp.org.br      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 20:24:03 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/09/05 20:24:03 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/10/05 20:41:47 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 int	*ft_range(int start, int end);
 
-/* int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int *number = ft_range(0, -17);
+	int *number = ft_range(-1, -17);
 
 	for(int i = 0; i < 17; i++)
 		printf("%d\n", number[i]);
 	return (0);
-} */
+}
 
 int	*ft_range(int start, int end)
 {
@@ -32,6 +32,8 @@ int	*ft_range(int start, int end)
 
 	if (end < 0 && start == 0)
 		range = malloc(sizeof(int) * (end * -1));
+	else if (end < 0 && start < 0)
+		range = malloc(sizeof(int) * (((end) - (start)) * -1) + 1);
 	else
 		range = malloc(sizeof(int) * ((end - start) + 1));
 	count = 0;
