eff95eb7c49cddb8722041f0504ff2c7ce68d8eb
Vinicius Pereira <niviciusdev@gmail.com>
2023-10-21 19:38:28 -0300

    first_word

---
+++ rank2/n1/first_word/first_word.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		while (*argv[1] == ' ')
+			argv[1]++;
+		while ((*argv[1] >= 'a' && *argv[1] <= 'z') || (*argv[1] >= 'A' && \
+				*argv[1] <= 'Z'))
+			write(1, argv[1]++, 1);
+	}
+	write(1, "\n", 1);
+}
