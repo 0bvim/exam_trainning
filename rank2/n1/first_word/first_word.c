af4d710d75ddb8d251ad01d2cc12eda99d7032c7
Vinicius Pereira <niviciusdev@gmail.com>
2023-10-21 19:48:08 -0300

    first_word

--- rank2/n1/first_word/first_word.c
+++ rank2/n1/first_word/first_word.c
@@ -4,7 +4,7 @@ int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		while (*argv[1] == ' ')
+		while (*argv[1] == ' ' || *argv[1] == '\t')
 			argv[1]++;
 		while ((*argv[1] >= 'a' && *argv[1] <= 'z') || (*argv[1] >= 'A' && \
 				*argv[1] <= 'Z'))
