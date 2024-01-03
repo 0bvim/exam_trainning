44d3094871019aeb75dbaf6846de83b7f19a19fd
vde-frei <vde-frei@student.42sp.org.br>
2024-01-03 15:39:53 -0300

    updating filse

---
+++ rank2/n2/snake_to_camel/test.sh
@@ -0,0 +1,8 @@
+cc -Wall -Wextra -Werror askjdaksjd.c -o aksjdaksjdk
+
+./snake_to_camel "here_is_a_snake_case_word"
+echo hereIsASnakeCaseWord
+./snake_to_camel "hello_world" | cat -e
+echo helloWorld$
+./snake_to_camel | cat -e
+echo $
