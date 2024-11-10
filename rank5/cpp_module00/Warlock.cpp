f80be6c972043d5c68faa8838efe946d996bb3c8
vinicius <niviciusdev@gmail.com>
2024-11-09 23:46:04 -0300

    chore finish mod 00

--- rank5/cpp_module00/Warlock.cpp
+++ rank5/cpp_module00/Warlock.cpp
@@ -1,32 +1,33 @@
 #include "Warlock.hpp"
 
-
-Warlock::Warlock(Warlock const & rhs){
-	*this = rhs;
-}
-
-Warlock &Warlock::operator=(Warlock const &rhs) {
-	if (this != &rhs)
-		*this = rhs;
-	return *this;
-}
-
 Warlock::Warlock(){}
 
+Warlock::~Warlock(){
+	std::cout << _name << ": My job here is done!" << std::endl;
+}
+
 Warlock::Warlock(std::string const &name, std::string const &title) : _name(name), _title(title) {
-	std::cout << this->_name << ": This looks like another boring day." << std::endl;
-}
-
-Warlock::~Warlock() {
-	std::cout << this->_name << ": My job here is done!" << std::endl;
-}
-
-void	Warlock::introduce() const {
-	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+	std::cout << _name << ": This looks like another boring day." << std::endl;
 }
 
 std::string const &Warlock::getName() const { return _name; }
 
 std::string const &Warlock::getTitle() const { return _title; }
 
-void Warlock::setTitle(std::string const &newTitle) { this->_title = newTitle; }
+void Warlock::setTitle(std::string const &newTitle) { _title = newTitle; }
+
+Warlock::Warlock(Warlock const &rhs) {
+	*this = rhs;
+}
+
+Warlock &Warlock::operator=(Warlock const &rhs) {
+	if (this != &rhs) {
+		this->_name = rhs._name;
+		this->_title = rhs._title;
+	}
+	return *this;
+}
+
+void Warlock::introduce() const {
+	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
--- rank5/cpp_module00/Warlock.hpp
+++ rank5/cpp_module00/Warlock.hpp
@@ -1,24 +1,19 @@
-#ifndef WARLOCK_HPP
-#define WARLOCK_HPP
-
 #include <iostream>
 
 class Warlock {
 	private:
 		std::string _name;
 		std::string _title;
-		Warlock(Warlock const & rhs);
+		Warlock(Warlock const &rhs);
 		Warlock &operator=(Warlock const &rhs);
 		Warlock();
 
 	public:
 		Warlock(std::string const &name, std::string const &title);
 		~Warlock();
-		std::string const	&getName() const;
-		std::string const	&getTitle() const;
-		void	setTitle(std::string const &newTitle);
-		void	introduce() const;
 
+		std::string const &getName() const;
+		std::string const &getTitle() const;
+		void setTitle(std::string const &newTitle);
+		void introduce() const;
 };
-
-#endif
